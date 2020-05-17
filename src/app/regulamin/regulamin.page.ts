2cb1e34f2d7a3da401ab92e63693d3aba585edba
ghostkillerpl <mrjanek1998@wp.pl>
2020-05-17 13:51:54 +0200

    commit on master

--- src/app/regulamin/regulamin.page.ts
+++ src/app/regulamin/regulamin.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-regulamin',
@@ -7,7 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegulaminPage implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    ) { }
+
+    Login() {
+      this.router.navigate(['/']);
+    }
 
   ngOnInit() {
   }
